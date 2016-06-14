10a80b0443b8e7efb48e31bb7534483e0ced039e
adzhu <adzhu@WASYGSHA02-1032>
2016-06-14 14:18:19 +0800

    change

--- Sites/OpenData.Site.FrontPage/Controllers/HomeController.cs
+++ Sites/OpenData.Site.FrontPage/Controllers/HomeController.cs
@@ -1,35 +1,188 @@
-﻿using System;
+﻿using OpenData.Framework.Core;
+using OpenData.Script;
+using OpenData.Framework.WebApp.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
 
-namespace OpenData.Site.FrontPage.Controllers
+namespace OpenData.Framework.WebApp.Controllers.Connect
 {
-    public class HomeController : Controller
+    public class HomeController : BzwayController
     {
-        public IActionResult Index()
+        public ActionResult Index()
         {
-            return View();
+            return View("Login");
+        }
+        public ActionResult Frame(string url)
+        {
+            return Content(string.Format(@"<iframe src='{0}' style='width: 100%; height:99%; border: none; ' frameborder='0' scrolling='auto'></iframe>", url));
+        }
+        public void Modules()
+        {
+            HttpApplication httpApps = ControllerContext.HttpContext.ApplicationInstance;
+            // 获取所有 http module
+            HttpModuleCollection httpModules = httpApps.Modules;
+
+            Response.Write(string.Format("一共有{0}个 HttpModule</br>", httpModules.Count.ToString()));
+            foreach (string activeModule in httpModules.AllKeys)
+            {
+                Response.Write(activeModule + "</br>");
+            }
         }
 
-        public IActionResult About()
+        public ActionResult upload()
         {
-            ViewData["Message"] = "Your application description page.";
 
-            return View();
+
+
+            var r = new List<ViewDataUploadFilesResult>();
+
+            foreach (string file in Request.Files)
+            {
+                var statuses = new List<ViewDataUploadFilesResult>();
+                var headers = Request.Headers;
+
+                if (string.IsNullOrEmpty(headers["X-File-Name"]))
+                {
+                    UploadWholeFile(Request, statuses);
+                }
+                else
+                {
+                    UploadPartialFile(headers["X-File-Name"], Request, statuses);
+                }
+
+                JsonResult result = Json(statuses);
+                result.ContentType = "text/plain";
+
+                return result;
+            }
+
+            return Json(r);
         }
 
-        public IActionResult Contact()
+        [HttpGet]
+        public void Delete(string id)
         {
-            ViewData["Message"] = "Your contact page.";
+            var filename = id;
+            var filePath = Server.MapPath("~/upload/" + filename);
 
-            return View();
+            if (System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Delete(filePath);
+            }
         }
 
-        public IActionResult Error()
+        //DONT USE THIS IF YOU NEED TO ALLOW LARGE FILES UPLOADS
+        [HttpGet]
+        public void Download(string id)
         {
-            return View();
+            var filename = id;
+            var filePath = Server.MapPath("~/upload/" + filename);
+
+            var context = HttpContext;
+
+            if (System.IO.File.Exists(filePath))
+            {
+                context.Response.AddHeader("Content-Disposition", "attachment; filename=\"" + filename + "\"");
+                context.Response.ContentType = "application/octet-stream";
+                context.Response.ClearContent();
+                context.Response.WriteFile(filePath);
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+            }
+        }
+
+        private string EncodeFile(string fileName)
+        {
+            return Convert.ToBase64String(System.IO.File.ReadAllBytes(fileName));
+        }
+
+        //DONT USE THIS IF YOU NEED TO ALLOW LARGE FILES UPLOADS
+        //Credit to i-e-b and his ASP.Net uploader for the bulk of the upload helper methods - https://github.com/i-e-b/jQueryFileUpload.Net
+        private void UploadPartialFile(string fileName, HttpRequestBase request, List<ViewDataUploadFilesResult> statuses)
+        {
+            if (request.Files.Count != 1) throw new HttpRequestValidationException("Attempt to upload chunked file containing more than one fragment per request");
+            var file = request.Files[0];
+            var inputStream = file.InputStream;
+
+            var fullName = Server.MapPath("~/upload/" + fileName);
+
+
+            using (var fs = new FileStream(fullName, FileMode.Append, FileAccess.Write))
+            {
+                var buffer = new byte[1024];
+
+                var l = inputStream.Read(buffer, 0, 1024);
+                while (l > 0)
+                {
+                    fs.Write(buffer, 0, l);
+                    l = inputStream.Read(buffer, 0, 1024);
+                }
+                fs.Flush();
+            }
+            statuses.Add(new ViewDataUploadFilesResult()
+            {
+                name = fileName,
+                size = file.ContentLength,
+                type = file.ContentType,
+                url = "/Home/Download/" + fileName,
+                delete_url = "/Home/Delete/" + fileName,
+                thumbnail_url = @"data:image/png;base64," + EncodeFile(fullName),
+                delete_type = "GET",
+            });
+        }
+
+        //DONT USE THIS IF YOU NEED TO ALLOW LARGE FILES UPLOADS
+        //Credit to i-e-b and his ASP.Net uploader for the bulk of the upload helper methods - https://github.com/i-e-b/jQueryFileUpload.Net
+        private void UploadWholeFile(HttpRequestBase request, List<ViewDataUploadFilesResult> statuses)
+        {
+            for (int i = 0; i < request.Files.Count; i++)
+            {
+                var file = request.Files[i];
+
+                var fullPath = Server.MapPath("~/upload/" + file.FileName);
+
+                file.SaveAs(fullPath);
+
+                statuses.Add(new ViewDataUploadFilesResult()
+                {
+                    name = file.FileName,
+                    size = file.ContentLength,
+                    type = file.ContentType,
+                    url = "/Home/Download/" + file.FileName,
+                    delete_url = "/Home/Delete/" + file.FileName,
+                    thumbnail_url = @"data:image/png;base64," + EncodeFile(fullPath),
+                    delete_type = "GET",
+                });
+            }
+        }
+        public ActionResult Execute(ExecuteModel model)
+        {
+            if (string.IsNullOrEmpty(model.Js))
+            {
+                model = new ExecuteModel();
+            }
+            else
+            {
+                model.Output = JavascriptExecuter.Exec(model.Js, model.Input);
+            }
+            return View(model);
         }
     }
+
+    public class ViewDataUploadFilesResult
+    {
+        public string name { get; set; }
+        public int size { get; set; }
+        public string type { get; set; }
+        public string url { get; set; }
+        public string delete_url { get; set; }
+        public string thumbnail_url { get; set; }
+        public string delete_type { get; set; }
+    }
+
 }
--- Sites/OpenData.Site.FrontPage/Startup.cs
+++ Sites/OpenData.Site.FrontPage/Startup.cs
@@ -1,92 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using OpenData.Site.FrontPage.Data;
-using OpenData.Site.FrontPage.Models;
-using OpenData.Site.FrontPage.Services;
-using OpenData.Framework.Core;
+﻿using Microsoft.Owin;
+using Owin;
 
-namespace OpenData.Site.FrontPage
+[assembly: OwinStartupAttribute(typeof(OpenData.Framework.WebApp.Startup))]
+namespace OpenData.Framework.WebApp
 {
-    public class Startup
+    public partial class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public void Configuration(IAppBuilder app)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
-
-            if (env.IsDevelopment())
-            {
-                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
-                builder.AddUserSecrets();
-            }
-
-            builder.AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
-
-        public IConfigurationRoot Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Add framework services.
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
-
-            services.AddMvc();
-
-            // Add application services.
-            services.AddTransient<IEmailSender, AuthMessageSender>();
-            services.AddTransient<ISmsSender, AuthMessageSender>();
-            services.AddTransient<ISiteService, SiteService>();
-            this.GloableServices = services;
-        }
-        IServiceCollection GloableServices { get; set; }
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-                app.UseBrowserLink();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            app.UseStaticFiles();
-
-            app.UseIdentity();
-
-            // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
-            app.UseMiddleware<UserSiteMiddleware>(this.GloableServices);
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.Use<FrontPageMiddleware>();
+            ConfigureAuth(app);
         }
     }
 }
